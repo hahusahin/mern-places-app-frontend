2af5f4d919eaf6f4edab87b31b62a7ce7d36b18f
hahusahin <hhsmobileapps@gmail.com>
2022-05-02 15:06:13 +0300

    frontend auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,21 +10,35 @@ import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
-          <Route path="/places/new" element={<NewPlace />} />
-          <Route path="/places/:placeId" element={<UpdatePlace />}/>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/:userId/places" element={<UserPlaces />} />
+            {isLoggedIn && <Route path="/places/new" element={<NewPlace />} />}
+            {isLoggedIn && <Route path="/places/:placeId" element={<UpdatePlace />} />}
+            {!isLoggedIn && <Route path="/auth" element={<Auth />} />}
+            {isLoggedIn && <Route path="/auth" element={<Navigate to="/" />} />}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
 
-import './NavLinks.css';
+import "./NavLinks.css";
 
-const NavLinks = props => {
-  return <ul className="nav-links">
-    <li>
-      <NavLink to="/">ALL USERS</NavLink>
-    </li>
-    <li>
-      <NavLink to="/u1/places">MY PLACES</NavLink>
-    </li>
-    <li>
-      <NavLink to="/places/new">ADD PLACE</NavLink>
-    </li>
-    <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
-    </li>
-  </ul>
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate()
+
+  const logout = () => {
+    auth.logout()
+    navigate("/")
+  }
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">ALL USERS</NavLink>
+      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <button onClick={logout}>LOGOUT</button>
+      )}
+    </ul>
+  );
 };
 
 export default NavLinks;
